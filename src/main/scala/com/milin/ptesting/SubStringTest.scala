e3d61fca849cf2e20344da60e8f2d2c31b7dd926
milinkp <milinkp@gmail.com>
2017-06-18 15:14:26 +0530

    added documentation

--- src/main/scala/com/milin/ptesting/SubStringTest.scala
+++ src/main/scala/com/milin/ptesting/SubStringTest.scala
@@ -8,12 +8,24 @@ import org.apache.spark.sql.functions._
   */
 object SubStringTest {
 
+  /**
+    * create a new column with the substring of given column ,
+    * if the provided searchword contains in the given input column.User can configure the substring
+    * using limit and offset parameter
+    *
+    * @param df         given dataframe
+    * @param nColName   new column name
+    * @param column     given column
+    * @param searchWord word to search
+    * @param limit      length of the output string
+    * @param offSet     offset of the output string
+    * @return dataframe having new column
+    */
 
-  def getSubText(df:DataFrame,nColName:String,column:String,searchWord:String,limit:Int,offSet:Int=0)={
-    df.withColumn(nColName,substring(
-      split(col(column),searchWord).getItem(1),offSet,limit))
+  def getSubText(df: DataFrame, nColName: String, column: String, searchWord: String, limit: Int, offSet: Int = 0) = {
+    df.withColumn(nColName, substring(
+      split(col(column), searchWord).getItem(1), offSet, limit))
     println("checking branch rebase")
     println("added some more changes")
   }
-
 }

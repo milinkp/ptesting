09fa1ad0dd74a48b7074f82eb738e90aefb3688b
milinkp <milinkp@gmail.com>
2017-06-18 15:16:30 +0530

    added some changes

--- src/main/scala/com/milin/ptesting/SubStringTest.scala
+++ src/main/scala/com/milin/ptesting/SubStringTest.scala
@@ -12,6 +12,7 @@ object SubStringTest {
   def getSubText(df:DataFrame,nColName:String,column:String,searchWord:String,limit:Int,offSet:Int=0)={
     df.withColumn(nColName,substring(
       split(col(column),searchWord).getItem(1),offSet,limit))
+    println("checking branch rebase")
   }
 
 }
